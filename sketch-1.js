d9d59e554924281b28e0f7e554a439095d26c3d8
ngkerry <31704416+ngkerry@users.noreply.github.com>
2017-10-01 15:00:39 -0700

    Create sketch-1.js

---
+++ sketch-1.js
@@ -0,0 +1,30 @@
+var x1 = 10; 
+var x2 = 10; 
+
+while (true){
+line (x1, 0, x2, height);
+  x1 = x1 + 10; 
+  x2 = x2 + 10; 
+}
+
+line(10, 0, 10, height);
+line(20, 0, 20, height);
+line(30, 0, 30, height);
+line(40, 0, 40, height);
+line(50, 0, 50, height);
+line(60, 0, 60, height);
+line(70, 0, 70, height);
+line(80, 0, 80, height);
+line(90, 0, 90, height);
+line(100, 0, 100, height);
+line(110, 0, 110, height);
+line(120, 0, 120, height);
+line(130, 0, 130, height);
+line(140, 0, 140, height);
+line(150, 0, 150, height);
+line(160, 0, 160, height);
+line(170, 0, 170, height);
+line(180, 0, 180, height);
+line(190, 0, 190, height);
+line(200, 0, 200, height);
+line(210, 0, 210, height);
