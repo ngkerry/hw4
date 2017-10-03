f51be4994619c2bdd6726e7fe9ca323a7f100e70
ngkerry <31704416+ngkerry@users.noreply.github.com>
2017-10-02 22:42:52 -0700

    Create sketch-8.js

---
+++ sketch-8.js
@@ -0,0 +1,171 @@
+var a = 15
+var b = 15
+var c = 15
+
+while ( a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width)
+{
+var c = 35;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 55;
+var a = 15;
+}
+
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width){
+var c = 75;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 95;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width)
+{
+var c = 115;
+}
+
+while (a >= 15){
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 135;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width){
+var c = 155;
+}
+
+while (a >= 15){
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 175;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+ if (a >= width){
+var c = 195;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 215;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width)
+{
+var c = 235;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 255;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width)
+{
+var c = 275;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5){
+var c = 295;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a =a + 20;
+}
+
+if (a >= width)
+{
+var c = 315;
+}
+
+while (a >= 15)
+{
+ellipse (a, c, b, b);
+var a = a - 20;
+}
+if (a >= -5)
+{
+var c = 335;
+var a = 15;
+}
+while (a <= width)
+{
+ellipse (a, c, b, b);
+var a = a + 20;
+}
+
+if (a >= width)
+{
+var c = 375;
+}
